fa08804da4980c4539dff1830e27ef7e65391777
Peter Asplund <peter.azp@gmail.com>
2020-05-27 12:29:45 +0200

    Run pre-commit-hooks on all files
    Results in reformatting and clean-up.

--- rsstorrent.py
+++ rsstorrent.py
@@ -1,4 +1,5 @@
 #! /usr/bin/python3
+# -*- coding: utf-8 -*-
 
 """RssTorrent.py: Monitors an RSS-feed and automatically
     downloads torrents to a specified folder."""
@@ -12,7 +13,7 @@ __maintainer__ = "Peter Asplund"
 __email__ = "peterasplund@gentoo.se"
 __status__ = "Stable"
 
-#Standard includes
+# Standard includes
 import os
 import signal
 import urllib.request
@@ -24,16 +25,19 @@ import re
 import logging
 import configparser
 import argparse
-#Extra packages
+
+# Extra packages
 import feedparser
 import daemon
-#import lockfile
+
+# import lockfile
 
 RUNNING = True
 
 
 class Child:
     """ Child process """
+
     child_id = ""
     pid = ""
     is_alive = False
@@ -54,6 +58,7 @@ class Child:
 
 class Environment:
     """ Keeps track of all files and directories """
+
     # File names
     cache_file = "cache"
     config_file = "rsstorrent.conf"
@@ -70,13 +75,11 @@ class Environment:
     config_dir_path = ""
 
     def __init__(self):
-        self.home_dir = os.path.expanduser('~')
+        self.home_dir = os.path.expanduser("~")
         self.cache_dir_path = os.path.join(self.home_dir + self.config_dir)
         self.config_dir_path = os.path.join(self.home_dir + self.config_dir)
-        self.cache_file_path = os.path.join(self.config_dir_path +
-                                            self.cache_file)
-        self.config_file_path = os.path.join(self.config_dir_path +
-                                             self.config_file)
+        self.cache_file_path = os.path.join(self.config_dir_path + self.cache_file)
+        self.config_file_path = os.path.join(self.config_dir_path + self.config_file)
         self.download_dir = os.path.join(self.home_dir + "/Download")
         if not os.path.exists(self.config_dir_path):
             os.mkdir(self.config_dir_path, 0o755)
@@ -94,6 +97,7 @@ class Environment:
 
 class Site:
     """ Represents a monitored site """
+
     feed_url = ""
     login_url = ""
     time_interval = 5.0
@@ -116,29 +120,31 @@ class Site:
 
 
 def create_config_file(cfg_file):
-    "("" Generate an empty config file """
-    with open(cfg_file, 'w+') as cfg_file_handle:
+    """ Generate an empty config file """
+    with open(cfg_file, "w+") as cfg_file_handle:
         # Split the file by lines to get rid of whitespace
-        lines = ["[General]\n",
-                 "# Directory which to download torrents to\n",
-                 "download_dir = /home/username/Downloads/\n",
-                 "\n",
-                 "[Site1]\n",
-                 "# Interval between checks in minutes\n",
-                 "interval = 30\n",
-                 "\n",
-                 "# URL to rss feed\n",
-                 "rss_url = http://www.urltosite.com\n",
-                 "\n",
-                 "# URL to site login page/script\n",
-                 "login_url = http://www.urltosite.com/takelogin.php\n",
-                 "\n",
-                 "# Search keys for the parsing\n",
-                 "keys = keys*to*search*for separated*by spaces\n",
-                 "\n",
-                 "# Username and Password to torrent site\n",
-                 "username = username\n",
-                 "password = password\n"]
+        lines = [
+            "[General]\n",
+            "# Directory which to download torrents to\n",
+            "download_dir = /home/username/Downloads/\n",
+            "\n",
+            "[Site1]\n",
+            "# Interval between checks in minutes\n",
+            "interval = 30\n",
+            "\n",
+            "# URL to rss feed\n",
+            "rss_url = http://www.urltosite.com\n",
+            "\n",
+            "# URL to site login page/script\n",
+            "login_url = http://www.urltosite.com/takelogin.php\n",
+            "\n",
+            "# Search keys for the parsing\n",
+            "keys = keys*to*search*for separated*by spaces\n",
+            "\n",
+            "# Username and Password to torrent site\n",
+            "username = username\n",
+            "password = password\n",
+        ]
         for line in lines:
             cfg_file_handle.writelines(line)
 
@@ -182,29 +188,31 @@ def site_login(site):
     cookie_jar = http.cookiejar.CookieJar()
 
     # build opener with HTTPCookieProcessor
-    opener = urllib.request.build_opener(
-        urllib.request.HTTPCookieProcessor(cookie_jar))
-    opener.addheaders.append(('User-agent',
-                              ('Mozilla/5.0 (X11; Linux x86_64; rv:2.0.1)'
-                               'Gecko/20110524 Firefox/4.0.1')))
+    opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cookie_jar))
+    opener.addheaders.append(
+        (
+            "User-agent",
+            (
+                "Mozilla/5.0 (X11; Linux x86_64; rv:2.0.1)"
+                "Gecko/20110524 Firefox/4.0.1"
+            ),
+        )
+    )
     urllib.request.install_opener(opener)
 
     # assuming the site expects 'user' and 'pass' as query params
-    login_query = urllib.parse.urlencode({
-        'username': site.username,
-        'password': site.password,
-        'login':    'Log in!'}).encode()
+    login_query = urllib.parse.urlencode(
+        {"username": site.username, "password": site.password, "login": "Log in!"}
+    ).encode()
 
     # perform login with params
     try:
         file_handle = opener.open(site.login_url, login_query)
         file_handle.close()
     except urllib.error.HTTPError as exception:
-        logging.error("HTTP Error: " + exception.code +
-                      " Site:" + site.feed_url)
+        logging.error("HTTP Error: " + exception.code + " Site:" + site.feed_url)
     except urllib.error.URLError as exception:
-        logging.error("URL Error: " + exception.reason +
-                      " Site:" + site.feed_url)
+        logging.error("URL Error: " + exception.reason + " Site:" + site.feed_url)
 
 
 def update_list_from_feed(url, regexp_keys):
@@ -213,21 +221,21 @@ def update_list_from_feed(url, regexp_keys):
 
     # Get feed
     feed = feedparser.parse(url)
-    if 'title' not in feed.feed:
+    if "title" not in feed.feed:
         logging.error("Error connecting to feed")
         return found_items
     else:
-        logging.info("Updating Feed: " + feed['feed']['title'])
+        logging.info("Updating Feed: " + feed["feed"]["title"])
 
     # Loop through that list
     for key in regexp_keys:
-        for item in feed['items']:
-            if key.search(item['title']):
-                logging.debug("Found match: " + item['title'])
-                logging.debug("\t" + item['link'])
-                found_items[item['title']] = item['link']
-                logging.debug("Added: " + found_items[item['title']])
-    logging.debug("Updated Feed: " + feed['feed']['title'])
+        for item in feed["items"]:
+            if key.search(item["title"]):
+                logging.debug("Found match: " + item["title"])
+                logging.debug("\t" + item["link"])
+                found_items[item["title"]] = item["link"]
+                logging.debug("Added: " + found_items[item["title"]])
+    logging.debug("Updated Feed: " + feed["feed"]["title"])
     return found_items
 
 
@@ -239,12 +247,10 @@ def process_download_list(cache, download_dir, input_list, options):
             logging.error("Can't find cache directory")
             return
         else:
-            logging.debug(
-                "No cache file was found, but caching was ignored anyway"
-            )
+            logging.debug("No cache file was found, but caching was ignored anyway")
 
     # Open cache file and start downloading
-    with open(cache, 'a+') as cache_file_handle:
+    with open(cache, "a+") as cache_file_handle:
         # Split the file by lines to get rid of whitespace
         cached_files = cache_file_handle.read().splitlines()
         for title in input_list:
@@ -253,8 +259,10 @@ def process_download_list(cache, download_dir, input_list, options):
             logging.debug("Processing: " + http_url)
             logging.debug("Filename resolved to: " + filename)
             if len(filename) < 1:
-                logging.critical("I was not able to find you a filename!\
-                The file cannot be saved!")
+                logging.critical(
+                    "I was not able to find you a filename!\
+                The file cannot be saved!"
+                )
                 continue
 
             if (filename in cached_files) and not options.cache_ignore:
@@ -274,7 +282,7 @@ def process_download_list(cache, download_dir, input_list, options):
                 logging.info("URL Redirect - Not allowed to download")
                 continue
 
-            with open(os.path.join(download_dir, filename), 'w') as local_file:
+            with open(os.path.join(download_dir, filename), "w") as local_file:
                 local_file.write(request.read())
 
             logging.info("Download successful: " + filename)
@@ -293,9 +301,9 @@ def convert_keys_to_regexps(sites):
 
 def setup_logging(env, options):
     """ Setup logging to file or tty. """
-    log_file = ''
+    log_file = ""
     # Logging format for logfile and console messages
-    formatting = '%(asctime)s (%(process)d) %(levelname)s: %(message)s'
+    formatting = "%(asctime)s (%(process)d) %(levelname)s: %(message)s"
 
     # set log filepath
     if options.log_file:
@@ -307,14 +315,13 @@ def setup_logging(env, options):
     # It is important to define the basic file
     # logging before the console logging
     if options.debug:
-        logging.basicConfig(filename=log_file, format=formatting,
-                            level=logging.DEBUG)
+        logging.basicConfig(filename=log_file, format=formatting, level=logging.DEBUG)
     elif options.verbose:
-        logging.basicConfig(filename=log_file, format=formatting,
-                            level=logging.INFO)
+        logging.basicConfig(filename=log_file, format=formatting, level=logging.INFO)
     else:
-        logging.basicConfig(filename=log_file, format=formatting,
-                            level=logging.CRITICAL)
+        logging.basicConfig(
+            filename=log_file, format=formatting, level=logging.CRITICAL
+        )
 
     # Always print messages to the console
     # In normal operating mode only CRITICAL messages will be displayed
@@ -328,7 +335,7 @@ def setup_logging(env, options):
         console.setLevel(logging.CRITICAL)
 
     console.setFormatter(logging.Formatter(formatting))
-    logging.getLogger('').addHandler(console)
+    logging.getLogger("").addHandler(console)
 
     return log_file
 
@@ -344,31 +351,81 @@ def parse_cmd_arguments():
     """ Parse command line arguments """
     # Parse command line commands
     parser = argparse.ArgumentParser()
-    parser.add_argument("-v", "--verbose", action="store_true", dest="verbose",
-                        help="Verbose logging", default=False)
-    parser.add_argument("-d", "--debug", action="store_true", dest="debug",
-                        help="Print debug information to console", default=False)
-    parser.add_argument("-1", "--run-once", action="store_true", dest="run_once",
-                        help="Run once, then exit", default=False)
-    parser.add_argument("-D", "--daemon", action="store_true", dest="daemon",
-                        help="Run as daemon", default=False)
-    parser.add_argument("-x", "--stop", action="store_true", dest="stop",
-                        help="Stop running daemon (does not work currently)",
-                        default=False)
-    parser.add_argument("-l", "--logfile", dest="log_file",
-                        help="Write log to FILE", metavar="FILE")
-    parser.add_argument("-p", "--pidfile", dest="pid_file",
-                        help="Set pidfile to FILE", metavar="FILE",
-                        default='/var/run/rsstorrent/rsstorrent.pid')
-    parser.add_argument("--cc", "--cache-clear", action="store_true",
-                        dest="cache_clear", help="Clear the cache file", default=False)
-    parser.add_argument("--ci", "--cache-ignore", action="store_true",
-                        dest="cache_ignore",
-                        help="Ignore the cache and download all files again",
-                        default=False)
-    parser.add_argument("--nd", "--no-downloads", action="store_true",
-                        dest="no_downloads",
-                        help="Don't actually download the files", default=False)
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="store_true",
+        dest="verbose",
+        help="Verbose logging",
+        default=False,
+    )
+    parser.add_argument(
+        "-d",
+        "--debug",
+        action="store_true",
+        dest="debug",
+        help="Print debug information to console",
+        default=False,
+    )
+    parser.add_argument(
+        "-1",
+        "--run-once",
+        action="store_true",
+        dest="run_once",
+        help="Run once, then exit",
+        default=False,
+    )
+    parser.add_argument(
+        "-D",
+        "--daemon",
+        action="store_true",
+        dest="daemon",
+        help="Run as daemon",
+        default=False,
+    )
+    parser.add_argument(
+        "-x",
+        "--stop",
+        action="store_true",
+        dest="stop",
+        help="Stop running daemon (does not work currently)",
+        default=False,
+    )
+    parser.add_argument(
+        "-l", "--logfile", dest="log_file", help="Write log to FILE", metavar="FILE"
+    )
+    parser.add_argument(
+        "-p",
+        "--pidfile",
+        dest="pid_file",
+        help="Set pidfile to FILE",
+        metavar="FILE",
+        default="/var/run/rsstorrent/rsstorrent.pid",
+    )
+    parser.add_argument(
+        "--cc",
+        "--cache-clear",
+        action="store_true",
+        dest="cache_clear",
+        help="Clear the cache file",
+        default=False,
+    )
+    parser.add_argument(
+        "--ci",
+        "--cache-ignore",
+        action="store_true",
+        dest="cache_ignore",
+        help="Ignore the cache and download all files again",
+        default=False,
+    )
+    parser.add_argument(
+        "--nd",
+        "--no-downloads",
+        action="store_true",
+        dest="no_downloads",
+        help="Don't actually download the files",
+        default=False,
+    )
     options = parser.parse_args()
 
     return options
@@ -378,7 +435,7 @@ def check_output_files(env, options):
     """ Check sanity of output directory and handle cache cleans """
     if options.cache_clear:
         # clear cache file
-        open(env.cache_file_path, 'w').close()
+        open(env.cache_file_path, "w").close()
         logging.info("Cleared cache")
         exit(0)
     if not os.path.exists(env.download_dir):
@@ -397,8 +454,12 @@ def do_main_program():
     if not os.path.exists(env.config_file_path):
         create_config_file(env.config_dir_path + env.config_file)
         logging.warning("There was no config file found, I just created one.")
-        logging.critical("Please check " + env.config_dir_path +
-                         env.config_file + " before restarting!")
+        logging.critical(
+            "Please check "
+            + env.config_dir_path
+            + env.config_file
+            + " before restarting!"
+        )
         exit(-1)
 
     sites = []
@@ -433,15 +494,12 @@ def initiate_daemon(options, env, log_file_path):
     """ Set up daemon context and return it """
     logging.debug("Setting up daemon with pid file: " + str(options.pid_file))
     # Set up some Daemon stuff
-    context = daemon.DaemonContext(
-        umask=0o002,
-        pidfile=options.pid_file,
-        )
+    context = daemon.DaemonContext(umask=0o002, pidfile=options.pid_file)
     context.signal_map = {
         signal.SIGTERM: cleanup_program,
         signal.SIGUSR1: cleanup_program,
-        signal.SIGHUP: cleanup_program
-        }
+        signal.SIGHUP: cleanup_program,
+    }
 
     if not context:
         logging.critical("Unable to create daemon context. Exiting")
@@ -463,22 +521,23 @@ def initiate_daemon(options, env, log_file_path):
     # Check if the daemon is already running
     # or at least if it has left a pid file behind
     try:
-        open(options.pid_file + '.lock', 'r')
+        open(options.pid_file + ".lock", "r")
         logging.info("pid-file exists, exiting")
         exit(-1)
     except IOError:
         pass
 
     # Open all important files and list them
-    cache_file_handle = open(env.cache_file_path, 'a+')
-    config_file_handle = open(env.config_file_path, 'a+')
+    cache_file_handle = open(env.cache_file_path, "a+")
+    config_file_handle = open(env.config_file_path, "a+")
     if log_file_path:
         log_file_handle = logging.root.handlers[0].stream.fileno()
-        logging.debug("Adding logging handle to files_preserve: "
-                      + log_file_path)
-        context.files_preserve = [cache_file_handle,
-                                  config_file_handle,
-                                  log_file_handle]
+        logging.debug("Adding logging handle to files_preserve: " + log_file_path)
+        context.files_preserve = [
+            cache_file_handle,
+            config_file_handle,
+            log_file_handle,
+        ]
     else:
         context.files_preserve = [cache_file_handle, config_file_handle]
     return context
@@ -490,10 +549,12 @@ def child_process_loop(env, site, options):
         # in child process
         download_list = update_list_from_feed(site.feed_url, site.regexp_keys)
         if len(download_list):
-            logging.debug("Start downloading, I found " +
-                          str(len(download_list)) + " items.")
-            process_download_list(env.cache_file_path,
-                                  env.download_dir, download_list, options)
+            logging.debug(
+                "Start downloading, I found " + str(len(download_list)) + " items."
+            )
+            process_download_list(
+                env.cache_file_path, env.download_dir, download_list, options
+            )
         time.sleep(site.time_interval)
     logging.debug("Exiting child process")
     exit(0)
--- setup.py
+++ setup.py
@@ -5,29 +5,29 @@
 from setuptools import setup
 from rsstorrent import __version__
 
-with open('README.md') as readme:
+with open("README.md") as readme:
     setup(
-        name='rsstorrent',
+        name="rsstorrent",
         version=__version__,
-        description='Monitors an RSS-feed and automatically '
-                    'downloads torrents to a specified folder.',
+        description="Monitors an RSS-feed and automatically "
+        "downloads torrents to a specified folder.",
         long_description=readme.read(),
-        license='GPL-3',
-        author='Peter Asplund',
-        author_email='peterasplund@gentoo.se',
-        url='https://github.com/AzP/rsstorrent',
+        license="GPL-3",
+        author="Peter Asplund",
+        author_email="peterasplund@gentoo.se",
+        url="https://github.com/AzP/rsstorrent",
         classifiers=[
-            'Development Status :: 5 - Production/Stable',
-            'Environment :: Console',
-            'Intended Audience :: End Users/Desktop',
-            'License :: OSI Approved :: GPL-3 License',
-            'Operating System :: OS Independent',
-            'Programming Language :: Python',
-            'Programming Language :: Python :: 2',
-            'Programming Language :: Python :: 3',
+            "Development Status :: 5 - Production/Stable",
+            "Environment :: Console",
+            "Intended Audience :: End Users/Desktop",
+            "License :: OSI Approved :: GPL-3 License",
+            "Operating System :: OS Independent",
+            "Programming Language :: Python",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
         ],
-        py_modules = ['rsstorrent'],
-        keywords='automation, p2p, network',
-        install_requires=['python-daemon', 'feedparser'],
-        entry_points={'console_scripts': ['rsstorrent = rsstorrent:do_main_program']},
+        py_modules=["rsstorrent"],
+        keywords="automation, p2p, network",
+        install_requires=["python-daemon", "feedparser"],
+        entry_points={"console_scripts": ["rsstorrent = rsstorrent:do_main_program"]},
     )
